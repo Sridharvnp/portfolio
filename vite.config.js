a53633627c4190e8598a89af2baaf0604715ab55
SRIDHAR VADIVEL <srilittle.sparkle@gmail.com>
2025-06-01 23:44:09 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio/",  // ✅ Your repo name here
+  base: "/portfolio/", 
   plugins: [react()],
-})
+});
